ad1a0b2f012565487f5a92dd0133bfec608092fd
VicBVic <0.BotnaruVictor.0@gmail.com>
2022-11-04 13:16:33 +0200

    morbin commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'dart:io';
+import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:fuzzy/fuzzy.dart';
 import 'package:google_ml_kit_example/bluetooth_views/device_select_screen.dart';
 import 'package:google_ml_kit_example/util/show_view.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:path_provider/path_provider.dart';
 import 'bluetooth_backend/blue_broadcast_handler.dart';
 import 'error_menus/no_bluetooth_menu.dart';
 import 'redux/bluetooth_reducer.dart';
@@ -13,6 +18,9 @@ import 'redux/bluetooth_state.dart';
 import 'redux/bluetooth_state_actions.dart';
 import 'vision_detector_views/text_detector_view.dart';
 import 'package:redux/redux.dart';
+
+import 'dart:ui' as UI;
+import 'package:async/async.dart';
 
 List<CameraDescription> cameras = [];
 
@@ -63,9 +71,40 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final TextRecognizer _textRecognizer = TextRecognizer();
   Uint8List? imageBytes;
   int imageLength = 0;
   List<Uint8List> chunks = List.empty(growable: true);
+  final imageSizeNames = {
+    1: "240x240",
+    2: "QVGA(320x240)",
+    3: "SVGA(800x600)",
+    4: "HD(1280,720)",
+    5: "QXGA(2048,1356)"
+  };
+  final imageSizes = {
+    1: [240, 240],
+    2: [320, 240],
+    3: [800, 600],
+    4: [1280, 720],
+    5: [2048, 1356]
+  };
+  int pickedImageSize = 1;
+
+  int picSeconds = 5;
+  int moveSeconds = 2;
+  int moveTimes = 5;
+
+  List<String> imageFoundText = List.empty(growable: true);
+
+  String searchingFor = "";
+  double foundConfidence = -1;
+
+  @override
+  void dispose() async {
+    _textRecognizer.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -82,9 +121,10 @@ class _HomeState extends State<Home> {
               padding: EdgeInsets.symmetric(horizontal: 16),
               child: StoreBuilder<BluetoothAppState>(builder: (context, store) {
                 return Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
                         ElevatedButton(
                           onPressed: () async {
@@ -116,21 +156,111 @@ class _HomeState extends State<Home> {
                         ),
                       ],
                     ),
+                    Text("Pic Seconds:"),
+                    Slider(
+                      value: picSeconds.toDouble(),
+                      max: 10,
+                      min: 1,
+                      divisions: 10,
+                      label: picSeconds.toString(),
+                      onChanged: (double value) {
+                        setState(() {
+                          picSeconds = value.round();
+                        });
+                      },
+                    ),
+                    Text("Move Seconds:"),
+                    Slider(
+                      value: moveSeconds.toDouble(),
+                      max: 10,
+                      min: 1,
+                      divisions: 10,
+                      label: moveSeconds.toString(),
+                      onChanged: (double value) {
+                        setState(() {
+                          moveSeconds = value.round();
+                        });
+                      },
+                    ),
+                    Text("Move Times:"),
+                    Slider(
+                      value: moveTimes.toDouble(),
+                      max: 10,
+                      min: 1,
+                      divisions: 10,
+                      label: moveTimes.toString(),
+                      onChanged: (double value) {
+                        setState(() {
+                          moveTimes = value.round();
+                        });
+                      },
+                    ),
+                    MotorControlRow(
+                      store: store,
+                    ),
                     TextFormField(
                       decoration:
                           InputDecoration(hintText: "choose a book name"),
+                      onChanged: (value) {
+                        setState(() {
+                          searchingFor = value;
+                        });
+                      },
+                    ),
+                    DropdownButton<int>(
+                      value: pickedImageSize,
+                      items: imageSizes.keys
+                          .map(
+                            (e) => DropdownMenuItem<int>(
+                              child: Text(imageSizeNames[e]!),
+                              value: e,
+                            ),
+                          )
+                          .toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          pickedImageSize = value ?? pickedImageSize;
+                        });
+                      },
                     ),
                     ElevatedButton(
                         onPressed: () {
-                          startCameraSearch(store);
+                          takeCameraPic(store, Duration(seconds: picSeconds));
                         },
-                        child: Text("Run!")),
+                        child: Text("Capture photo")),
                     imageBytes == null
                         ? Container()
                         : Image.memory(
                             imageBytes!,
                             fit: BoxFit.cover,
                           ),
+                    imageFoundText.length > 0
+                        ? Text(imageFoundText.fold(
+                            "Found text: ",
+                            (previousValue, element) =>
+                                previousValue + element))
+                        : Container(),
+                    foundConfidence > 0
+                        ? Text(
+                            "Found book with confidence: $foundConfidence",
+                            style: TextStyle(color: Colors.green),
+                          )
+                        : Container(),
+                    ElevatedButton(
+                        onPressed: () async {
+                          final text =
+                              await runVisionAI("/bruh.jpeg", imageBytes!);
+                          setState(() {
+                            imageFoundText.clear();
+                            imageFoundText.add(text);
+                          });
+                        },
+                        child: Text("Run Vision AI!")),
+                    ElevatedButton(
+                        onPressed: () {
+                          runFullApp(store);
+                        },
+                        child: Text("Run Full App!")),
                     ExpansionTile(
                       title: const Text('Vision APIs'),
                       children: [
@@ -150,9 +280,79 @@ class _HomeState extends State<Home> {
     );
   }
 
-  void startCameraSearch(Store<BluetoothAppState> store) async {
+  double getFuzzyMatch(String main, String pattern) {
+    Fuzzy fuzzy = Fuzzy(imageFoundText,
+        options: FuzzyOptions(tokenize: false, threshold: 0.5));
+    final res = fuzzy.search(pattern);
+    double result = 0;
+    for (var r in res) {
+      result = max(result, r.score);
+    }
+    return result;
+  }
+
+  Future<void> runFullApp(Store<BluetoothAppState> store) async {
+    imageFoundText.clear();
+    for (int i = 0; i < moveTimes; i++) {
+      Future<void> moveMotor = Future.delayed(Duration(milliseconds: 500)).then(
+          (value) async =>
+              await turnMotor(store, false, Duration(seconds: moveSeconds)));
+      Future<void> takePic = takeCameraPic(store, Duration(seconds: picSeconds))
+          .then((value) async {
+        print("maiking request!");
+        final text = await runVisionAI("/bruh$i.jpeg", imageBytes!);
+        print("finished request!");
+        setState(() {
+          imageFoundText.add(text);
+        });
+      });
+      //FutureGroup group = FutureGroup();
+      //group.add(moveMotor);
+      //group.add(takePic);
+      //group.close();
+      await takePic;
+      await moveMotor;
+      print("finished a cycle!");
+    }
+  }
+
+  Future<String> runVisionAI(String imagePath, Uint8List bytes) async {
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    //  UI.ImageDescriptor imd =
+    //      await UI.ImageDescriptor.encoded(
+    //          await UI.ImmutableBuffer.fromUint8List(
+    //              imageBytes!));
+    //  UI.Codec codec = await imd.instantiateCodec();
+    //  UI.FrameInfo frame = await codec.getNextFrame();
+    //  UI.Image image = frame.image;
+    File img = File("${appDocDir.path}${imagePath}");
+    if (img.existsSync()) await img.delete();
+    img = await img.create();
+    img.writeAsBytesSync(bytes.toList());
+    final recognizedText =
+        await _textRecognizer.processImage(InputImage.fromFile(img));
+    if (searchingFor != "")
+      setState(() {
+        foundConfidence = getFuzzyMatch(recognizedText.text, searchingFor);
+      });
+    return recognizedText.text;
+  }
+
+  Future<void> turnMotor(
+      Store<BluetoothAppState> store, bool left, Duration time) async {
+    BluetoothConnection connection = store.state.motor!;
+    BlueBroadcastHandler.instance.printMessage(connection, left ? "<" : ">");
+    await Future.delayed(time);
+    print("stopped motor");
+    BlueBroadcastHandler.instance.printMessage(connection, "V");
+  }
+
+  Future<void> takeCameraPic(
+      Store<BluetoothAppState> store, Duration timeout) async {
+    imageLength = 0;
+    chunks.clear();
     BluetoothConnection connection = store.state.camera!;
-    BlueBroadcastHandler.instance.printMessage(connection, "p");
+    BlueBroadcastHandler.instance.printMessage(connection, "p$pickedImageSize");
     var sub = BlueBroadcastHandler.instance
         .getCommandStreamController(connection)
         .stream
@@ -163,7 +363,7 @@ class _HomeState extends State<Home> {
         imageLength += event.length;
       });
     });
-    await Future.delayed(Duration(seconds: 5));
+    await Future.delayed(timeout);
     print("Hopefully all data was received!");
     sub.cancel();
     buildImage();
@@ -171,13 +371,60 @@ class _HomeState extends State<Home> {
 
   void buildImage() {
     if (chunks.isEmpty || imageLength == 0) return;
-    imageBytes = Uint8List(imageLength);
+    setState(() {
+      imageBytes = Uint8List(imageLength);
+    });
     int offset = 0;
     for (var chunk in chunks) {
-      imageBytes!.setRange(offset, offset + chunk.length, chunk);
+      setState(() {
+        imageBytes!.setRange(offset, offset + chunk.length, chunk);
+      });
       offset += chunk.length;
     }
-    setState(() {});
+  }
+}
+
+class MotorControlRow extends StatelessWidget {
+  final Store<BluetoothAppState> store;
+  const MotorControlRow({
+    Key? key,
+    required this.store,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            BluetoothConnection? connection = store.state.motor;
+            if (connection != null) {
+              BlueBroadcastHandler.instance.printMessage(connection, "<");
+            }
+          },
+          child: Text("<"),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            BluetoothConnection? connection = store.state.motor;
+            if (connection != null) {
+              BlueBroadcastHandler.instance.printMessage(connection, "V");
+            }
+          },
+          child: Text("V"),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            BluetoothConnection? connection = store.state.motor;
+            if (connection != null) {
+              BlueBroadcastHandler.instance.printMessage(connection, ">");
+            }
+          },
+          child: Text(">"),
+        ),
+      ],
+    );
   }
 }
 
